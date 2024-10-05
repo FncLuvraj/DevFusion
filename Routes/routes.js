d95de3d30fca7ef308057d97777907599ad5b81c
Luvraj Singh <98079619+FncLuvraj@users.noreply.github.com>
2024-10-05 18:44:01 +0530

    Added patch API for updating user information

--- Routes/routes.js
+++ Routes/routes.js
@@ -3,8 +3,13 @@ const router = express.Router();
 
 const signup = require("../Controllers/signup");
 const login = require("../Controllers/login");
+const {
+  updateUserInfo,
+  updateUserValidationRules,
+} = require("../Controllers/updateUserInfo");
 
 router.post("/signup", signup);
 router.post("/login", login);
+router.patch("/update", updateUserValidationRules, updateUserInfo);
 
 module.exports = router;
